06b3aa734743c31e0888f70da0d41e007067a330
angel85luz <penaflores.miguelangel@gmail.com>
2026-01-09 12:29:59 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+/* Minimal service worker for offline cache */
+const CACHE_NAME = "ab-campo-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) =>
+      cached ||
+      fetch(req)
+        .then((res) => {
+          if (req.method === "GET") {
+            const copy = res.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(() => {});
+          }
+          return res;
+        })
+        .catch(() => cached)
+    )
+  );
+});
